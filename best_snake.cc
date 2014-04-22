c67f19fae4c791e792bbd9405bc1930d3bffbca1
Ting Xu <ting@idea>
2014-04-22 15:40:25 -0400

    Added best_snake.cc

--- best_snake.cc
+++ best_snake.cc
@@ -3,32 +3,33 @@
 #include <vector>
 #include <map>
 #include <fstream>
-#include <iomanip>
+// #include <iomanip>
 #include <boost/program_options.hpp>
 #include <boost/filesystem.hpp>
 #include "multisnake.h"
-#include "utility.h"
+// #include "utility.h"
 
 typedef std::map<std::string, std::vector<double> > FValuesMap;
-typedef std::map<double, std::pair<double, double> > TErrorMap;
 typedef std::map<double, std::string> TFilenameMap;
-std::vector<double> ComputeFValueVector(soax::Multisnake &ms,
+std::vector<double> ComputeFValueVector(soax::Multisnake &ms, int rnear, int rfar,
                                         double start, double end, double step);
-void PrintMap(const FValuesMap &fmap);
+void PrintFMap(const FValuesMap &fmap);
 void PrintVector(const std::vector<double> &v);
-void PrintThresholdErrorMap(const TErrorMap &m, const TFilenameMap &tfm, std::ostream &os);
+void PrintTFilenameMap(const TFilenameMap &tfm, std::ostream &os);
 std::string GetMinimalErrorFilename(const FValuesMap &m, unsigned index);
 
+
+
 int main (int argc, char **argv) {
-  if (argc < 4) {
-    std::cerr << "./bestt <image_path> <gt_path> <snake_dir> " << std::endl;
+  if (argc < 6) {
+    std::cerr << "./best_soacs <image_path> <snake_dir> "
+        "<radial_near> <radial_far> <output_path> " << std::endl;
     return EXIT_FAILURE;
   }
 
   namespace fs = boost::filesystem;
   std::string image_path = argv[1];
-  std::string gt_path = argv[2];
-  fs::path snake_dir(argv[3]);
+  fs::path snake_dir(argv[2]);
 
   try {
     if (fs::exists(snake_dir)) {
@@ -43,20 +44,18 @@ int main (int argc, char **argv) {
                 back_inserter(sorted_snakes_path));
       std::sort(sorted_snakes_path.begin(), sorted_snakes_path.end());
 
-      double t_start = 6.0;
-      double t_end = 12.9;
+      double t_start = 1.0;
+      double t_end = 5.01;
       double t_step = 0.2;
+      int rnear = std::atoi(argv[3]);
+      int rfar = std::atoi(argv[4]);
       for (Paths::const_iterator it = sorted_snakes_path.begin();
            it != sorted_snakes_path.end(); ++it) {
         std::cout << it->filename() << std::endl;
         ms.LoadConvergedSnakes(it->string());
-        fmap[it->string()] = ComputeFValueVector(ms, t_start, t_end, t_step);
+        fmap[it->filename().string()] = ComputeFValueVector(
+            ms, rnear, rfar, t_start, t_end, t_step);
       }
-
-      TErrorMap t_error_map; // threshold - Hausdorff distance/Vertex error map
-      ms.LoadGroundTruthSnakes(gt_path);
-      std::cout << ms.GetNumberOfComparingSnakes1()
-                << " ground truth snakes loaded." << std::endl;
 
       unsigned num_entries = static_cast<unsigned>((t_end - t_start) / t_step)+1;
 
@@ -66,16 +65,11 @@ int main (int argc, char **argv) {
         double t = t_start + t_step * i;
         t_filenames[t] = filename;
         // std::cout << filename << std::endl;
-        ms.LoadConvergedSnakes(filename);
-        double vertex_error(100.0), hausdorff(100.0);
-        ms.ComputeResultSnakesVertexErrorHausdorffDistance(vertex_error, hausdorff);
-        // std::cout << hausdorff << std::endl;
-        t_error_map[t] = std::make_pair(vertex_error, hausdorff);
       }
 
-      // PrintMap(fmap);
-      std::ofstream outfile("t_error.txt");
-      PrintThresholdErrorMap(t_error_map, t_filenames, outfile);
+      // PrintFMap(fmap);
+      std::ofstream outfile(argv[5]);
+      PrintTFilenameMap(t_filenames, outfile);
     } else {
       std::cout << snake_dir << " does not exist." << std::endl;
     }
@@ -86,33 +80,28 @@ int main (int argc, char **argv) {
 }
 
 
-std::vector<double> ComputeFValueVector(soax::Multisnake &ms,
+std::vector<double> ComputeFValueVector(soax::Multisnake &ms, int rnear, int rfar,
                                         double start, double end, double step) {
-  const double c = 2.0;
+  const double c = 1.1;
+  // const int rnear = 4;
+  // const int rfar = 12;
   std::vector<double> v;
   for (double t = start; t < end; t += step) {
     soax::DataContainer snrs;
-    ms.ComputeResultSnakesLocalSNRs(4, 12, snrs);
+    ms.ComputeResultSnakesLocalSNRs(rnear, rfar, snrs);
     v.push_back(ms.ComputeFValue(snrs, t, c));
   }
   return v;
 }
 
-void PrintThresholdErrorMap(const TErrorMap &m, const TFilenameMap &tfm,
-                            std::ostream &os) {
-  for (TErrorMap::const_iterator it = m.begin();
-       it != m.end(); ++it) {
-    os << it->first << ": [" << it->second.first << ' '
-       << it->second.second << "]" << std::endl;
-  }
-
+void PrintTFilenameMap(const TFilenameMap &tfm, std::ostream &os) {
   for (TFilenameMap::const_iterator it = tfm.begin();
        it != tfm.end(); ++it) {
     os << it->first << ": " << it->second << std::endl;
   }
 }
 
-void PrintMap(const FValuesMap &fmap) {
+void PrintFMap(const FValuesMap &fmap) {
   for (FValuesMap::const_iterator it = fmap.begin();
        it != fmap.end(); ++it) {
     std::cout << it->first << ": ";
--- multisnake.cc
+++ multisnake.cc
@@ -26,7 +26,7 @@ namespace soax {
 Multisnake::Multisnake() : image_(NULL), external_force_(NULL),
                            intensity_scaling_(0.0), sigma_(1.0),
                            ridge_threshold_(0.01), foreground_(65535),
-                           background_(0), initialize_z_(true),
+                           background_(0), initialize_z_(false),
                            is_2d_(false) {
   interpolator_ = InterpolatorType::New();
   vector_interpolator_ = VectorInterpolatorType::New();
